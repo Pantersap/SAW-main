04fc6714ae87c06376a778df331eb045bd05b5e6
Pantersap <stefan.v.d.berg@outlook.com>
2024-01-18 15:17:15 +0100

    met classes

--- TEST.py
+++ TEST.py
@@ -1,36 +1,44 @@
 from random import choice
 from matplotlib import pyplot as plt
 plt.style.use('Solarize_Light2')
+import copy
+
 
 class SAW():
-    def __init__(self,SAW_Length):
-        x, y = 0, 0
-        path = [(x, y)] # het pad als koppeltjes
-    #2D self-avoiding random walk
-    def __add__(self, other): #length of path
-        while len(path)<=SAW_Length:
+    def __init__(self, path=[(0,0)]):
+        SAW.path = path # het pad als koppeltjes, meegegeven als je niks hebt meegegeven
+    #2D self-avoiding random walk laten groeien
+    def __add__(self, other): #self = length of path and other is how much you grow it by
+        self2 = copy.copy(self) #kopiërt de self
+        CurrentPoint=self2.path[-1]
+        x, y = CurrentPoint[0], CurrentPoint[1]
+        while len(self2.path)<=other: 
             # pick the closest point  without crossing itself
             u, v = choice([(x+1, y), (x-1, y), (x, y+1), (x, y-1)]) #finds a random path
-            if (x+1, y) in path and (x-1, y) in path and (x, y+1) in path and (x, y-1) in path: #if there are no possible paths left
-                return path 
-            elif (u, v) not in path: #if the random generated path doesn't cross itself
+            if (x+1, y) in self2.path and (x-1, y) in self2.path and (x, y+1) in self2.path and (x, y-1) in self2.path: #if there are no possible paths left
+                return self2 
+            elif (u, v) not in self2.path: #if the random generated path doesn't cross itself
                 x, y = u, v #swaps back to our original values
-                path.append((x, y)) #new piece of path added
-        return path
+                self2.path.append((x, y)) #new piece of path added
+        return self2
 
     #show plot
-    def show_path(path): 
+    def __pos__(self): # het tonen van de SAW 
         plt.figure(figsize=(100, 100))
         # draw points
-        plt.scatter(*zip(*path), s=10, c='k')
+        plt.scatter(*zip(*self.path), s=10, c='k')
         # draw lines in red
-        plt.plot(*zip(*path), c='r')
+        plt.plot(*zip(*self.path), c='r')
         plt.show()
 
-    NewSaw=SAW()
-    path=SAW.pathing(100000)
-    #main
     
-    if __name__ == '__main__': 
-        path = pathing(100000) #decides the length of the saw
-        show_path(path)
+#main
+    
+#if __name__ == '__main__': 
+# path = pathing(100000) #decides the length of the saw
+# show_path(path)
+        
+Newsaw = SAW([(69,420)])
+Updatedsaw = Newsaw+50
+Updatedsaw.__pos__()
+#Updatedsaw = Newsaw+10
