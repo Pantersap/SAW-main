6c6f27fedba8a74156b354a719e9bebd09f69fa3
Pantersap <stefan.v.d.berg@outlook.com>
2024-01-21 21:50:29 +0100

    fatsoenlijke saw square

--- TEST.py
+++ TEST.py
@@ -3,6 +3,13 @@ from matplotlib import pyplot as plt
 plt.style.use('Solarize_Light2')
 import copy
 
+def show(self): # het tonen van de SAW 
+        plt.figure(figsize=(100, 100))
+        # draw points
+        plt.scatter(*zip(*self), s=10, c='k')
+        # draw lines in red
+        plt.plot(*zip(*self), c='r')
+        plt.show()
 
 class SAW():
     def __init__(self, path=[(0,0)]):
@@ -10,14 +17,23 @@ class SAW():
     #2D self-avoiding random walk laten groeien
     def __add__(self, other): #self = length of path and other is how much you grow it by
         self2 = copy.copy(self) #kopiërt de self
+        NewMaxLength = other+len(self2.path) #nieuwe maximale lengte berekenen we nu
         CurrentPoint=self2.path[-1]
         x, y = CurrentPoint[0], CurrentPoint[1]
-        while len(self2.path)<=other: 
+        k=1
+        illegal = [] #the one iteration illegal maker, to make sure that backtracking works and the SAW doesn't take the same path twice in a row
+        while len(self2.path)<NewMaxLength: 
+            options = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)] #all the potential options
             # pick the closest point  without crossing itself
-            u, v = choice([(x+1, y), (x-1, y), (x, y+1), (x, y-1)]) #finds a random path
-            if (x+1, y) in self2.path and (x-1, y) in self2.path and (x, y+1) in self2.path and (x, y-1) in self2.path: #if there are no possible paths left
-                return self2 
-            elif (u, v) not in self2.path: #if the random generated path doesn't cross itself
+            aoptions = [] # allowed options list
+            for i in range(0,len(options)):
+                if options[i] not in self2.path and options[i] not in illegal:
+                    aoptions.append(options[i]) # stopt ze in de toegestaande opties
+            if len(aoptions)==0: #if there are no possible paths left before our SAW got a length of n, we're gonna backtrack as much as necessary
+                illegal.append((x,y)) #makes this point illegal for 1 iteration so that it wont literally take this path again
+                del self2.path[-1] # deletes your currentpoint from self2.path (your SAW's path)   
+            else: # if there are options
+                u, v = choice(aoptions) #finds a random path from the possible options
                 x, y = u, v #swaps back to our original values
                 self2.path.append((x, y)) #new piece of path added
         return self2
@@ -31,14 +47,13 @@ class SAW():
         plt.plot(*zip(*self.path), c='r')
         plt.show()
 
-    
 #main
     
 #if __name__ == '__main__': 
 # path = pathing(100000) #decides the length of the saw
 # show_path(path)
         
-Newsaw = SAW([(69,420)])
-Updatedsaw = Newsaw+50
+Newsaw = SAW()
+Updatedsaw = Newsaw+20
 Updatedsaw.__pos__()
 #Updatedsaw = Newsaw+10
